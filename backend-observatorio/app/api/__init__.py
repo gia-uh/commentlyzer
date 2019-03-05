95481027aef827bb8a9fd1de57455d1039556f44
hian <hiancdtrsnm@gmail.com>
2019-03-05 21:05:14 +0000

    Added Front Working

--- backend-observatorio/app/api/__init__.py
+++ backend-observatorio/app/api/__init__.py
@@ -1,15 +1,29 @@
 from flask import Blueprint,request
 from flask_cors import CORS
+import os
+import importlib
 
 api = Blueprint('api', __name__)
 CORS(api)
 
-# from .fake import comments, sumarization
 from . import crawler,  article, task
+#comments = None
+#sumarization = None
+task = None
+#from .fake import comments, sumarization
 
-from .fake import comments, sumarization
-#from . import comments, sumarization
+real = os.getenv('REAL_PROCESS')
 
+if True:
+    #global comments, sumarization
+    #comments = importlib.import_module('comments','.')
+    #sumarization = importlib.import_module('sumarization','.')
+    exec('from . import comments, sumarization',globals())
+else:
+    #global comments, sumarization
+    #comments = importlib.import_module('comments','.fake')
+    #sumarization = importlib.import_module('sumarization','.fake')
+    exec('from .fake import comments, sumarization',globals())
 
 @api.before_request
 def before_request():
